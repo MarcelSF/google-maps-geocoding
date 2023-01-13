9abe9f72303fb3779f9a549b12f5b58bbaff026c
Marcel Fonseca <marcel@lewagon.org>
2023-01-13 09:49:56 +0100

    Add google maps JS package

--- app/controllers/flats_controller.rb
+++ app/controllers/flats_controller.rb
@@ -4,6 +4,15 @@ class FlatsController < ApplicationController
   # GET /flats or /flats.json
   def index
     @flats = Flat.all
+    @markers = @flats.map do |flat|
+      {
+        lat: flat.latitude,
+        lng: flat.longitude#,
+        # infoWindow: { content: render_to_string(partial: "/flats/map_box", locals: { flat: flat }) }
+        # Uncomment the above line if you want each of your markers to display a info window when clicked
+        # (you will also need to create the partial "/flats/map_box")
+      }
+    end
   end
 
   # GET /flats/1 or /flats/1.json
